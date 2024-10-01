3bcd282074e37c848ddcf1fb069f5ecc064b3c69
jlefevre559 <jlefevre559@gmail.com>
2024-10-01 16:24:17 -0600

    10/1/24 background pic, documentation

--- application/Resume/urls.py
+++ application/Resume/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path, include
 from Resume.views import (
     Index,
+    ResumeView,
 )
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("", Index.as_view(), name="Index"),
+    path("resume/", ResumeView.as_view(), name="Resume"),
 ]
--- application/Resume/views.py
+++ application/Resume/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "index.html"
+
+class ResumeView(TemplateView):
+    template_name = "resume/resume.html"
